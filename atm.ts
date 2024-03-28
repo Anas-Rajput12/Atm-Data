2bf43eea94ad9b0e00a0f5235c036dae6eaf374c
Muhammad Anas Qadri <161422002+Anas-Rajput12@users.noreply.github.com>
2024-03-28 22:53:31 +0500

    Update atm.ts

--- atm.ts
+++ atm.ts
@@ -1,3 +1,4 @@
+#!  /usr/bin/env node
 import inquirer from "inquirer";
 import chakAnimation from "chalk-animation";
 
@@ -7,82 +8,84 @@ const sleep = ()=>{
     })}
   
 async function atm() {
-    let rainbow = chakAnimation.rainbow(`let Start Atm`);
+    let rainbow = chakAnimation.rainbow(`let Start Atm
+Hint PinCode is 4 Number And First Two Number is 23`);
     await sleep ();
     rainbow.stop();
 }
 atm();
-async function data () {
-    let atm1 = true;
+// console.log("Hint PinCode is 4 Number And First Two Number is 23");
+let mybalance =10000;
+let pinCode = 2345;
+async function data() {
+    let atm1 = true
     do {
-        const answer = await inquirer.prompt ([
+        let answer = await inquirer.prompt([
             {
-                name : "userId",
-                type: "input",
-                message : "Kindly enter Your Id"
-            },
-            {
-                name : "userPin",
-                type: "number",
-                message : "Kindly enter Your Pin"
-            },
-            {
-                type : "list",
-                name : "accountType",
-                choices : ["Current","Saving"],
-                message : "Enter Your AccountType",
-            },
-            {
-                type : "list",
-                name : "transactionType",
-                choices : ["fastCash","withdraw"],
-                message : "Enter Your TransactionType",
-                when(answer){
-                    return answer.accountType
-                },
-            },
-            {
-                type : "list",
-                name : "amount",
-                choices : ["1000","2000","10000","20000"],
-                message : "Enter Your amount",
-                when(answer){
-                    return answer.transactionType == "fastCash"
-                },
-            },
-            {
+                name : "pin",
                 type : "number",
-                name : "amount",
-                message : "Enter your amount",
-                when(answer){
-                    return answer.transactionType == "withdraw"
-                },
-            },
+                message : "enter your pin :",
+            }
         ]);
         
-        if (answer.userId && answer.userPin) {
-            const balance = Math.floor(Math.random() * 10000000);
-            console.log(balance);
-            const EnteredAmount = answer.amount;
-            if (balance >= EnteredAmount) {
-                const remaining = balance-EnteredAmount;
-                console.log("your remainig balance is ",remaining);
-            }else{
-                console.log("Insufficient balance!!!");
-            }
-            const {atmdata} = await inquirer.prompt([
+        if (answer.pin === pinCode) {
+            console.log("correct your pin");
+            let answer1 = await inquirer.prompt([
                 {
-                  name : "atmdata",
-                  type : "confirm",
-                  message : "Funther check your Transaction ?",
-                  default : true
+                    type : "list",
+                    name : "accountType",
+                    choices : ["Current","Saving"],
+                    message : "Enter Your AccountType",
+                },
+                {
+                    type : "list",
+                    name : "transactionType",
+                    choices : ["fastCash","withdraw"],
+                    message : "Enter Your TransactionType",
                 }
-              ]);
-              atm1 = atmdata;
-    
+            ]);
+            console.log(answer1);
+            if (answer1.transactionType === "withdraw") {
+                let answer2 = await inquirer.prompt([
+                    {
+                        type : "number",
+                        name : "amount",
+                        message : "Enter your amount",
+                    }
+                ]);
+                // console.log(answer2);
+                mybalance -=  answer2.amount;
+                console.log("your remaining balance is : " + mybalance); 
+            }else if (answer1.transactionType === "fastCash") {
+                let answer3 = await inquirer.prompt([
+                    {
+                        type : "list",
+                        name : "amount",
+                        choices : ["1000","2000","10000","20000"],
+                        message : "Enter Your amount : ",
+                    }
+                ]);
+                // console.log(answer3);
+                mybalance -=  answer3.amount;
+                console.log("your remaining balance is : " + mybalance);
+            }
         }
+        else{
+            console.log("Please enter Correct pin");
+        }
+        const {atmdata} = await inquirer.prompt([
+            {
+              name : "atmdata",
+              type : "confirm",
+              message : "Funther check your Transaction ?",
+              default : true
+            }
+          ]);
+          atm1 = atmdata;
     } while (atm1);
 }
 data();
+
+
 
 
